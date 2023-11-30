3007ed7ab4f65908a4b9d995dd3effea5db1a175
Valentyner <robertdoms2002@gmail.com>
2023-11-30 16:47:21 +0200

    back up

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          DON`T Learn React dfgsdfsdfgsdf
+          We are learning REACT
         </a>
       </header>
     </div>
