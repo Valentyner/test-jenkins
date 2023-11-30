1b986db4b5d5988a6e4cb13fbdc85e7806f8473d
Valentyner <robertdoms2002@gmail.com>
2023-11-30 22:18:05 +0200

    Final Test 1

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           rel="noopener noreferrer"
         >
           We are learning REACT
-          Final Test
+          Final Test 1
         </a>
       </header>
     </div>
